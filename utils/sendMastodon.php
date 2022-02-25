3b012fe50e7c8a860a567fb7ba672885e1cf38f6
Maurice Renck <maurice@maurice-renck.de>
2022-02-25 18:30:46 +0100

    feat: send toots

--- utils/sendMastodon.php
+++ utils/sendMastodon.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace mauricerenck\Komments;
+namespace mauricerenck\IndieConnector;
 
 use Kirby\Toolkit\Str;
 
@@ -8,33 +8,51 @@ class MastodonSender
 {
     public function sendToot($page)
     {
-        $tootMaxLength = 280;
-        $postUrl = $page->url();
-        $urlLength = Str::length($postUrl);
-        $trimTextPosition = $tootMaxLength - $urlLength - 2;
-        $textfield = option('mauricerenck.komments.mastodon-text-field', 'mastodonTeaser');
-        $message = ($page->$textfield()->isNotEmpty()) ? $page->$textfield() : Str::short($page->title(), $trimTextPosition);
-        $message .= ' ' . $postUrl;
+        if (!option('mauricerenck.indieConnector.mastodon-bearer', false)) {
+            return false;
+        }
 
-        $headers = [
-            'Authorization: Bearer ' . option('mauricerenck.komments.mastodon-bearer')
-        ];
+        if (!option('mauricerenck.indieConnector.mastodon-instance-url', false)) {
+            return false;
+        }
 
-        $status_data = [
-            'status' => $message,
-            'language' => 'de',
-            'visibility' => 'public'
-        ];
+        if (strpos($page->url(), '//localhost') === true || strpos($page->url(), '//127.0.0') === true) {
+            return false;
+        }
 
-        $ch_status = curl_init();
-        curl_setopt($ch_status, CURLOPT_URL, option('mauricerenck.komments.mastodon-instance-url'));
-        curl_setopt($ch_status, CURLOPT_POST, 1);
-        curl_setopt($ch_status, CURLOPT_POSTFIELDS, $status_data);
-        curl_setopt($ch_status, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch_status, CURLOPT_HTTPHEADER, $headers);
+        try {
+            $tootMaxLength = 280;
+            $postUrl = $page->url();
+            $urlLength = Str::length($postUrl);
+            $trimTextPosition = $tootMaxLength - $urlLength - 2;
+            $textfield = option('mauricerenck.indieConnector.mastodon-text-field', 'description');
+            $message = ($page->$textfield()->isNotEmpty()) ? $page->$textfield() : Str::short($page->title(), $trimTextPosition);
+            $message .= ' ' . $postUrl;
 
-        $output_status = json_decode(curl_exec($ch_status));
+            $headers = [
+                'Authorization: Bearer ' . option('mauricerenck.indieConnector.mastodon-bearer')
+            ];
 
-        curl_close($ch_status);
+            $status_data = [
+                'status' => $message,
+                // 'language' => 'de', // TODO
+                'visibility' => 'public'
+            ];
+
+            $ch_status = curl_init();
+            curl_setopt($ch_status, CURLOPT_URL, option('mauricerenck.indieConnector.mastodon-instance-url'));
+            curl_setopt($ch_status, CURLOPT_POST, 1);
+            curl_setopt($ch_status, CURLOPT_POSTFIELDS, $status_data);
+            curl_setopt($ch_status, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch_status, CURLOPT_HTTPHEADER, $headers);
+
+            $output_status = json_decode(curl_exec($ch_status));
+
+            curl_close($ch_status);
+
+            return true;
+        } catch (Exception $e) {
+            return false;
+        }
     }
 }
