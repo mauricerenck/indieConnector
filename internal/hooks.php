7e8e9db497bb6df32f5344bad7f270a1cca55f0f
Maurice Renck <maurice@maurice-renck.de>
2024-01-09 18:00:47 +0100

    improvement: hook - detect urls once

--- internal/hooks.php
+++ internal/hooks.php
@@ -5,15 +5,19 @@ namespace mauricerenck\IndieConnector;
 return [
     'page.update:after' => function ($newPage) {
         $webmentions = new WebmentionSender();
-        $webmentions->sendWebmentions($newPage);
+
+        $urls = $webmentions->getUnprocessedUrls($newPage);
+        $webmentions->sendWebmentions($newPage, $urls);
     },
 
     'page.changeStatus:after' => function ($newPage, $oldPage) {
-
         $webmentions = new WebmentionSender();
-        $webmentions->sendWebmentions($newPage);
+
+        $urls = $webmentions->getUnprocessedUrls($newPage);
+        $webmentions->sendWebmentions($newPage, $urls);
 
         if (option('mauricerenck.indieConnector.sendMastodon', false)) {
+            // FIXME use existing sender class for tests
             if (!$newPage->isDraft() && $oldPage->isDraft()) {
                 $mastodonSender = new MastodonSender();
                 $mastodonSender->sendToot($newPage);
@@ -25,5 +29,4 @@ return [
         $migrations = new Migrations();
         $migrations->migrate();
     }
-
 ];
