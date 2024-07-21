b093cdd107ea618fca81cdb700a35ea30e51b6e4
Maurice Renck <maurice@maurice-renck.de>
2024-07-21 17:12:09 +0200

    improvement: stats

--- src/components/DetailsByMonth.vue
+++ src/components/DetailsByMonth.vue
@@ -3,44 +3,46 @@
         <div class="wrapper">
             <k-headline size="large">Summary</k-headline>
             <k-line-field />
-            <table>
-                <tr>
-                    <td>
-                        {{ summary.summary }} <span v-if="summary.summary > 1">Webmentions</span
-                        ><span v-else-if="summary.summary === 1">Webmention</span>
-                    </td>
-                    <td>
-                        <div>
-                            <k-icon type="heart-filled" style="color: var(--color-red-700);" />
-                            {{ summary.likes }}
-                        </div>
-                    </td>
-                    <td>
-                        <div>
-                            <k-icon type="chat" style="color: var(--color-yellow-700);" />
-                            {{ summary.replies }}
-                        </div>
-                    </td>
-                    <td>
-                        <div>
-                            <k-icon type="indie-repost" style="color: var(--color-green-700);" />
-                            {{ summary.reposts }}
-                        </div>
-                    </td>
-                    <td>
-                        <div>
-                            <k-icon type="indie-mention" style="color: var(--color-blue-700);" />
-                            {{ summary.mentions }}
-                        </div>
-                    </td>
-                    <td>
-                        <div>
-                            <k-icon type="bookmark" style="color: var(--color-purple-700);" />
-                            {{ summary.bookmarks }}
-                        </div>
-                    </td>
-                </tr>
-            </table>
+            <div class="k-table">
+                <table>
+                    <tr>
+                        <th>
+                            {{ summary.summary }} <span v-if="summary.summary > 1">Webmentions</span
+                            ><span v-else-if="summary.summary === 1">Webmention</span>
+                        </th>
+                        <td>
+                            <div>
+                                <k-icon type="heart-filled" style="color: var(--color-red-700);" />
+                                {{ summary.likes }}
+                            </div>
+                        </td>
+                        <td>
+                            <div>
+                                <k-icon type="chat" style="color: var(--color-yellow-700);" />
+                                {{ summary.replies }}
+                            </div>
+                        </td>
+                        <td>
+                            <div>
+                                <k-icon type="indie-repost" style="color: var(--color-green-700);" />
+                                {{ summary.reposts }}
+                            </div>
+                        </td>
+                        <td>
+                            <div>
+                                <k-icon type="indie-mention" style="color: var(--color-blue-700);" />
+                                {{ summary.mentions }}
+                            </div>
+                        </td>
+                        <td>
+                            <div>
+                                <k-icon type="bookmark" style="color: var(--color-purple-700);" />
+                                {{ summary.bookmarks }}
+                            </div>
+                        </td>
+                    </tr>
+                </table>
+            </div>
         </div>
     </div>
 </template>
