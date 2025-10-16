1a603be5dbf963bbe1a0a36820c83209e6a7ff8d
Maurice Renck <maurice@maurice-renck.de>
2025-10-16 16:54:46 +0200

    fix: no more scss style lang

--- src/components/Queue.vue
+++ src/components/Queue.vue
@@ -30,7 +30,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="css">
 .k-webmentions-queue-view {
     .wrapper {
         background: #fff;
--- src/components/Sources.vue
+++ src/components/Sources.vue
@@ -170,7 +170,7 @@ export default {
     },
 }
 </script>
-<style lang="scss">
+<style lang="css">
 .k-webmentions-view {
     .source-entry {
         display: flex;
