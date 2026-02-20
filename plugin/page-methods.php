372ce7a89c7647e4e9875e88993bb64409421a9e
Maurice Renck <maurice@maurice-renck.de>
2026-02-20 14:42:09 +0100

    improvement: slimmed down page methods

--- plugin/page-methods.php
+++ plugin/page-methods.php
@@ -4,27 +4,9 @@ namespace mauricerenck\IndieConnector;
 
 return [
     'icGetMastodonUrl' => function () {
-        $mastodonSender = new MastodonSender();
-        return $mastodonSender->getPostTargetUrl('mastodon', $this);
-    },
-    'icGetMastodonPostData' => function () {
-        $mastodonSender = new MastodonSender();
-        $mastodonReceiver = new MastodonReceiver();
-        $responseCollector = new ResponseCollector();
-
-        $url = $mastodonSender->getPostTargetUrl('mastodon', $this);
-        $text = $mastodonReceiver->fetchMastodonPostText($url);
-        $stats = $responseCollector->getMastodonPostResponseStats($url);
-
-        $postData = [
-            'text' => $text,
-            'likes' => $stats['like-of'],
-            'reposts' => $stats['repost-of'],
-            'mentions' => $stats['mention-of'],
-            'replies' => $stats['in-reply-to']
-        ];
-
-        return new Content($postData);
+        $sender = new ExternalPostSender();
+        $data = $sender->getPostTargetUrlAndStatus('mastodon', $this);
+        return $data['url'];
     },
     'icGetBlueskyUrl' => function () {
         $bluesky = new Bluesky();
